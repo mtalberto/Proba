4f057c11d0ee9df03c64f4deebf8cf8289f3144d
WebProba <alberto.fachan@gmail.com>
2024-01-18 14:57:41 +0100

    Master

---
+++ web/src/main/java/com/app/proba/web/WebProfaApplication.java
@@ -0,0 +1,13 @@
+package com.app.proba.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebProfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebProfaApplication.class, args);
+	}
+
+}
---
+++ web/src/test/java/com/app/proba/web/WebProfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.proba.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
